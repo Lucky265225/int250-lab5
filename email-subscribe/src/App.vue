48fd1fa02696a6cbc7f9c12582aa9a1e66886d69
atiht dangpradub <Student@Train11-1-PC320.control.ad.sit.kmutt.ac.th>
2026-02-11 16:49:00 +0700

    lab5

---
+++ email-subscribe/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Card from './Card.vue';
+</script>
+
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-gray-700">
+    <Card/>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ email-subscribe/src/Card.vue
@@ -0,0 +1,30 @@
+<script setup>
+</script>
+
+<template>
+
+    <div class="flex items-center justify-center gap-10 bg-zinc-500 border-4 p-10 rounded-2xl">
+        <div>
+            <img src="https://cdn.shopify.com/s/files/1/2627/1860/files/understanding_matcha_powder_480x480.jpg?v=1643137921" alt=""
+            class="object-fit rounded-2xl h-80">
+        </div>
+        <div class="flex-col">
+            <h1 class="font-serif font-bold text-2xl mb-5 text-white">
+                Get diet and fitness tips in your inbox
+            </h1>
+            <p class="mb-5 text-xs text-white">
+                Eat better and exercise better. Sign up for the <br>Diet&Fitness newsletter.
+            </p>
+            <div>
+                <input type="text" placeholder="Enter your email address"
+                class="border-2 border-gray-600 rounded-xs mr-5 px-5">
+                <button class="bg-lime-400 px-5 rounded-md">Subscribe</button>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
